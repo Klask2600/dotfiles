04dee05f8b7283289481158259049e51ccaf0a49
Chris <chris@njinn.io>
2018-11-18 18:06:55 +0100

    made it work

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
+sudo pacman -Sy
 sudo pacman -S firefox firefox-adblock-plus termite polybar
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/chrishejl/dotfiles.git
-cp -rf dotfiles/* .
+yes | cp -rf dotfiles/. .
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
+chsh -s $(which zsh)
