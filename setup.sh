a7e32b42ae4a7ccc26e1708bc3b63f0fce2c527d
chrishejl <klask2600@gmail.com>
2018-11-18 21:34:46 +0100

    Added alt-tab for workspace switching

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ sudo pacman -S firefox firefox-adblock-plus termite polybar
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/chrishejl/dotfiles.git
 yes | cp -rf dotfiles/. ~
+rm -rf ~/.git
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 git clone https://github.com/pmbenjamin/purity.git
