0cfa31ea112e1c38d745f56b6813c36eb32fe084
chrishejl <chris@njinn.io>
2018-11-18 18:21:13 +0100

    update copy to always use home dir

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ sudo pacman -S firefox firefox-adblock-plus termite polybar
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/chrishejl/dotfiles.git
-yes | cp -rf dotfiles/. .
+yes | cp -rf dotfiles/. ~
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 chsh -s $(which zsh)
